6ed5c8ebdc9ecfbc2623e3a95ed24464bd932898
beniasta <beniastasmile@gmail.com>
2015-05-14 10:46:35 +0000

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   
   get 'users/show'
-
+  devise_for :users
   resources :users, only: [:show]
   
   # get 'static_pages/help'
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
    
   # get 'static_pages/contact'
   
-  devise_for :users
+  
   match '/help', to: 'static_pages#help',  via: 'get'
   match '/about', to: 'static_pages#about',  via: 'get'
   match '/contact', to: 'static_pages#contact',  via: 'get'
