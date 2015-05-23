5b4fb45b2cce7218f5fc24d81eaadb0d745d65a5
beniasta <beniastasmile@gmail.com>
2015-05-23 10:29:27 +0000

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
-Rails.application.routes.draw do
+SampleApp::Application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
+  resources :users, only: [:show]
+
   root 'static_pages#home'
   
   get 'users/show'
-  devise_for :users
-  resources :users, only: [:show]
   
   # get 'static_pages/help'
   
